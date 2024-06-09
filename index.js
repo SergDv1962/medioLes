775602aaa963e1f4ab40310c7f339de3aa04c666
unknown <dsv19621227@gmail.com>
2024-06-09 10:12:28 +0300

    передача файлу через multer({dest:...})

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const multer = require('multer');
 
 const app = express();
 const port = 3000;
 
+// dest - destanation -це куди будуть зберігатися файли. uploads/ - це назва папки uploads котра буде створена у каталозі нашого проекту і де будуть зберігатися загружені файли
+const upload = multer({dest: 'uploads/'}).single('demo_image');
+
 app.get('/', (req, res) => {
    console.log('Hello World!')
 });
+
+app.post('/image', (req, res) => {
+   upload(req, res, (err)=> {
+      if (err) return res.status(400).send('Something went wrong');
+
+      res.status(200).send(req.file)
+   })
+})
 
 app.listen(port, () => {
    console.log(`Server listening to port ${port}`)
