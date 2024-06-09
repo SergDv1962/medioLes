63b2fb174b8b895153f2a2927c66265a8871e4e8
unknown <dsv19621227@gmail.com>
2024-06-09 10:51:24 +0300

    multer.diskStorage для одного файлу

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
-const express = require('express');
-const multer = require('multer');
+const express = require("express");
+const multer = require("multer");
 
 const app = express();
 const port = 3000;
 
-// dest - destanation -це куди будуть зберігатися файли. uploads/ - це назва папки uploads котра буде створена у каталозі нашого проекту і де будуть зберігатися загружені файли
-const upload = multer({dest: 'uploads/'}).single('demo_image');
-
-app.get('/', (req, res) => {
-   console.log('Hello World!')
+// diskStorage
+// cb - name callback function
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
 
-app.post('/image', (req, res) => {
-   upload(req, res, (err)=> {
-      if (err) return res.status(400).send('Something went wrong');
+// limits це ліміт об'єму пам'ті котру користувач може використати 1Mb
+const upload = multer({ storage, limits: 1000000 }).single("demo_image");
 
-      res.status(200).send(req.file)
-   })
-})
+app.get("/", (req, res) => {
+  console.log("Hello World!");
+});
+
+app.post("/image", (req, res) => {
+  upload(req, res, (err) => {
+    if (err) return res.status(400).send("Something went wrong");
+
+    res.status(200).send(req.file);
+  });
+});
 
 app.listen(port, () => {
-   console.log(`Server listening to port ${port}`)
+  console.log(`Server listening to port ${port}`);
 });
